de5e86272192c14a24168590491cfe4f555bedcd
Santiago Ivulich <sivulich@itba.edu.ar>
2019-01-12 00:43:26 -0300

    Anda con file system

--- TP_Final_Labo_Micros/Main.cpp
+++ TP_Final_Labo_Micros/Main.cpp
@@ -2,6 +2,7 @@
 #include "UI.h"
 #include <conio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "./helix/pub/mp3dec.h"
 void printInfo(MP3FrameInfo info)
 {
@@ -16,48 +17,46 @@ void printInfo(MP3FrameInfo info)
 	);
 	return;
 }
-int main(void)
+int checkMP3file(char* fn, unsigned sz)
 {
-	char* currPos;
-	
-	char cmd = 0;
-	unsigned cnt=0,i=0;
-	currarr current;
-	UI.init();
+	if (fn[sz - 1] == '3' && fn[sz - 2] == 'p' && fn[sz - 3] == 'm'&& fn[sz - 4] == '.')
+		return 1;
+	return 0;
+}
+int handleMp3File(char* fn)
+{
 	HMP3Decoder dec = MP3InitDecoder();
 	MP3FrameInfo info;
-	FILE* fp,*fpo;
-	fopen_s(&fp,"Test.mp3", "rb");
+	FILE* fp, *fpo;
+	fopen_s(&fp, fn, "rb");
 	fopen_s(&fpo, "Out.out", "w");
 	if (fp == NULL)
 	{
 		printf("Couldnt open file\nPress any key to exit\n");
 		_getch();
-		return 0;
+		return 1;
 	}
-	printf("Opened MP3 file!\n");
 	unsigned char* mp3file;
 	short outBuff[4 * 1152];
-	int sz,frames=0,skipped=0;
+	int sz,lsz, frames = 0, skipped = 0;
 	fseek(fp, 0L, SEEK_END);
 	sz = ftell(fp);
 	rewind(fp);
-	printf("The Mp3 file has a size of: %d\nAttempting to allocate file size in ram\n",sz);
-	mp3file =(unsigned char*) malloc(sz);
+	mp3file = (unsigned char*)malloc(sz);
 	if (mp3file == NULL)
 	{
-		printf("Couldnt =(\nPress any key to exit\n");
+		printf("Couldnt allocate memory\nPress any key to continue\n");
 		_getch();
-		return 0;
+		return 1;
 	}
-	printf("Could =)\nTransfering file to memory:\n");
 	for (int i = 0; i < sz; i++)
 	{
 		mp3file[i] = fgetc(fp);
 		if (i % 100000 == 0)
 			putchar('#');
 	}
-	printf("\nFinished\nNow decoding and writing to file\n");
+	lsz = sz;
+	putchar('\n');
 	while (sz > 0)
 	{
 		int off = MP3FindSyncWord(mp3file, sz);
@@ -66,6 +65,11 @@ int main(void)
 		mp3file += off;
 		sz -= off;
 		int code = MP3Decode(dec, &mp3file, &sz, outBuff, 0);
+		if (sz < 0.9*lsz)
+		{
+			putchar('x');
+			lsz = sz;
+		}
 		if (code == 0)
 		{
 			MP3GetLastFrameInfo(dec, &info);
@@ -82,11 +86,8 @@ int main(void)
 					out += samp;
 				}
 				out /= info.nChans;
-				fprintf(fpo, "%d\n", out);
+				fprintf(fpo,"%d,", out);
 			}
-
-			printInfo(info);
-			printf("Size is now: %d\n", sz);
 			frames++;
 		}
 		else
@@ -95,24 +96,28 @@ int main(void)
 			{
 				mp3file += 209;
 				sz -= 209;
-				printf("Failed frame with code: %d\n",code);
-				//int off=MP3FindSyncWord(mp3file, sz);
-				//if (off == -1)
-					//break;
-				//mp3file += off;
-				//sz -= off;
+				printf("Failed frame with code: %d\n", code);
 				skipped++;
 			}
 			else
 				break;
 		}
-
-
 	}
-	printf("Decoded %d frames and skipped %d, %d bytes where left\n",frames,skipped,sz);
+	putchar('\n');
+	printInfo(info);
 	fclose(fp);
 	fclose(fpo);
-	/*while (cmd != 'r')
+}
+int main(void)
+{
+	char* currPos;
+	
+	char cmd = 0;
+	unsigned cnt=0,i=0;
+	currarr current;
+	UI.init();
+	
+	while (cmd != 'r')
 	{
 		UI.input(cmd);
 		current = UI.getCurrent(&cnt,&i);
@@ -133,11 +138,19 @@ int main(void)
 		else
 		{
 			printf("Selected the file %s\nPress any key to exit\n", currPos);
+			if (checkMP3file(currPos, strlen(currPos)))
+			{
+				printf("Decoding file:\n");
+				handleMp3File(currPos);
+				printf("Done\n");
+			}
+			else
+				printf("Not an MP3 file\n");
 			_getch();
 			UI.exitFile();
 			cmd = 0;
 		}
-	}*/
+	}
 	printf("Thanks for using MP3\n");
 	_getch();
 	return 0;
---
+++ TP_Final_Labo_Micros/Play.py
@@ -0,0 +1,15 @@
+import csv
+import sounddevice as sd
+import time
+f = open('Out.out','r')
+dat = csv.reader(f,delimiter=',')
+wav = []
+for x in dat:
+	for i in x:
+		if(i!=''):
+			wav.append(int(i))
+f.close()
+m = max(wav)
+wav = [x/m for x in wav]
+sd.play(wav,48000)
+time.sleep(60)
